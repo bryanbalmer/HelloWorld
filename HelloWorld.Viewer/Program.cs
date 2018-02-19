b1ba0572ecdee6098e0a4d731316768528347395
Bryan Balmer <bryanbalmer@live.com>
2018-02-19 16:27:28 -0600

    Implement console app.

--- HelloWorld.Viewer/Program.cs
+++ HelloWorld.Viewer/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Net.Http;
 
 namespace HelloWorld.Viewer
 {
@@ -6,7 +8,27 @@ namespace HelloWorld.Viewer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var builder = new ConfigurationBuilder().AddJsonFile("appsettings.json");
+            var config = builder.Build();
+            var baseApiUrl = config["baseApiUrl"];
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(baseApiUrl);
+
+                try
+                {
+                    var response = client.GetAsync("helloworld/").Result;
+                    var message = response.Content.ReadAsStringAsync().Result;
+                    Console.WriteLine(message);
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine($"Message not found. {e}");
+                }
+            }
+
+            Console.ReadKey();
         }
     }
 }
